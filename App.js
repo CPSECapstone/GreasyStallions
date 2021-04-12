a89870bd2fbcd2317e1236f95c21219a8780e2ae
DJ <33232754+djuliano14@users.noreply.github.com>
2021-04-11 22:31:40 -0700

    auth stuff

--- App.js
+++ App.js
@@ -1,226 +1,126 @@
-import React, { useState } from 'react';
-import { ScrollView, Button, Text, View, SafeAreaView, ActivityIndicator, StyleSheet, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Button, Linking, Platform, Text, View } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
 import { ApolloProvider, useQuery, gql} from '@apollo/client';
 import { Picker } from '@react-native-picker/picker';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigation from './navigation';
-import config from './aws-exports';
-import Amplify from '@aws-amplify/core';
+//import config from './aws-exports';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { apolloClient } from './apollo';
 import { apolloClientFlipted} from './apollo-flipted';
 
-Amplify.configure(config);
 
 const LIST_USERS = gql
-`
-    query{getUsers{fname lname}}
-`;
-/*
-const FliptedComponent = () => {
-  const {data, error, loading} = useQuery(LIST_USERS);
-  console.log(data);
-  //console.log(Object.keys(data));
-  //const user1 = data.getUsers
-  //const user1name = 'yo';
-  if (error) { console.log('Error fetching users', error); }
 
+async function urlOpener(url, redirectUrl) {
+    const { type, url: newUrl } = await WebBrowser.openAuthSessionAsync(
+        url,
+        redirectUrl
+    );
 
-  let students = [];
-  if(data){
-  data.getUsers.forEach( usr =>{
-    students.push(<Text style={styles.label}> {usr.fname + " " + usr.lname}</Text>)
-  });
-  }
-  
-  return (
-    <View style = {styles.container}>
-      {students}
-    </View>
-  );
-
-}*/
-/*
-export default function App() {
-  return (
-    <ApolloProvider client={apolloClientFlipted}>
-      <FliptedComponent />
-    </ApolloProvider>
-  );
-}*/
-// this is for the sign in stuff remove ^
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <AppNavigation />
-      <StatusBar style="auto" />
-    </View>
-  );
+    if (type === 'success' && Platform.OS === 'ios') {
+        WebBrowser.dismissBrowser();
+        return Linking.openURL(newUrl);
+    }
 }
 
-
-const styles = StyleSheet.create({
-  loadingContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 0,
-  },
-  label: {
-    marginBottom: 2,
-    fontSize: 12,
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-  },
-  section: {
-    marginVertical: 12,
-  },
-  starshipName: {
-    fontSize: 32,
-    fontWeight: 'bold',
-  },
-  starshipModel: {
-    fontStyle: 'italic',
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
+Amplify.configure({
+  Auth: {
+      identityPoolId: 'us-east-1:07057d76-612a-4045-8522-f38a759cf216',
+      region: 'us-east-1',
+      userPoolId: 'us-east-1_POfbbYTKF',
+      userPoolWebClientId: '24sdf1brebo58s89ja0b63c51d',
+      oauth: {
+        domain: 'flipted-ios-test.auth.us-east-1.amazoncognito.com',
+        scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+        redirectSignIn: 'http://localhost:3000/',
+        redirectSignOut: 'http://localhost:3000/',
+        responseType: 'token'
+      }
+  }
 });
 
+function App() {
+    const [user, setUser] = useState(null);
 
-// Imperial I-class Star Destroyer
-/*
-const defaultStarshipId = 'c3RhcnNoaXBzOjM=';
-*/
-/*
-function RootComponent() {
-  const [starshipId, setStarshipId] = useState(defaultStarshipId);
-  const { data, error, loading } = useQuery(GET_STARSHIP, {
-    variables: { id: starshipId },
+    useEffect(() => {
+        Hub.listen('auth', ({ payload: { event, data } }) => {
+            switch (event) {
+                case 'signIn':
+                    getUser().then((userData) => setUser(userData));
+                    break;
+                case 'signOut':
+                    setUser(null);
+                    break;
+                case 'signIn_failure':
+                case 'cognitoHostedUI_failure':
+                    console.log('Sign in failure', data);
+                    break;
+            }
+        });
+
+        getUser().then((userData) => setUser(userData));
+    }, []);
+
+    function getUser() {
+        return Auth.currentAuthenticatedUser()
+            .then((userData) => userData)
+            .catch(() => console.log('Not signed in'));
+    }
+
+    return (
+        <View>
+            <Text>User: {user ? JSON.stringify(user.attributes) : 'None'}</Text>
+            {user ? (
+                <Button title="Sign Out" onPress={() => Auth.signOut()} />
+            ) : (
+                <Button title="Federated Sign In" onPress={() => Auth.federatedSignIn()} />
+            )}
+            
+        </View>
+        
+    );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+    loadingContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      paddingHorizontal: 0,
+    },
+    label: {
+      marginBottom: 2,
+      fontSize: 12,
+      fontWeight: 'bold',
+      textTransform: 'uppercase',
+    },
+    section: {
+      marginVertical: 12,
+    },
+    starshipName: {
+      fontSize: 32,
+      fontWeight: 'bold',
+    },
+    starshipModel: {
+      fontStyle: 'italic',
+    },
+    item: {
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+    },
   });
-
-  if (error) { console.log('Error fetching starship', error); }
-
-  return (
-    <View style={styles.container}>
-
-      <View style={styles.section}>
-        Example app from react native template, leaving for syntax reference
-        <StarshipPicker
-          starshipId={starshipId}
-          onStarshipChange={setStarshipId}
-        />
-      </View>
-      {loading ? (
-        <ActivityIndicator color='#333' />
-      ) : (
-        <StarshipDetails starship={data.starship} />
-      )}
-    </View>
-  );
-}
-
-function StarshipPicker(props) {
-  const { data, error, loading } = useQuery(LIST_STARSHIPTS);
-
-  if (error) { console.log('Error listing starships', error) }
-  if (loading) return null;
-
-  const { starships } = data.allStarships;
-
-  return (
-    <Picker
-      selectedValue={props.starshipId}
-      onValueChange={props.onStarshipChange}
-    >
-      {starships.map(starship => (
-        <Picker.Item key={starship.id} label={starship.name} value={starship.id} />
-      ))}
-    </Picker>
-  )
-}
-
-function StarshipDetails({ starship }) {
-  return (
-    <>
-      <View style={styles.section}>
-        <Text style={styles.starshipName}>{starship.name}</Text>
-        <Text style={styles.starshipModel}>{starship.model}</Text>
-      </View>
-
-      <View style={styles.section}>
-        <Text style={styles.label}>Operational abilities</Text>
-        <Text>- {starship.crew} crew members</Text>
-        <Text>- {starship.consumables} without restocking</Text>
-      </View>
-
-      <View>
-        <Text style={styles.label}>Ship attributes</Text>
-        <Text>- {starship.length}m long</Text>
-        <Text>- {starship.costInCredits} credits</Text>
-      </View>
-
-      <View style={styles.section}>
-        <Text style={styles.label}>Manufacturers</Text>
-        {starship.manufacturers.map(manufacturer => (
-          <Text key={manufacturer}>- {manufacturer}</Text>
-        ))}
-      </View>
-
-      <View style={styles.section}>
-        <Text style={styles.label}>Appeared in</Text>
-        {starship.filmConnection.films.map(film => (
-          <Text key={film.id}>- {film.title}</Text>
-        ))}
-      </View>
-    </>
-  )
-}
-
-*/
-/*
-
-const LIST_STARSHIPTS = gql`
-  query listStarships {
-    allStarships {
-      starships {
-        id
-        name
-      }
-    }
-  }
-`;
-
-const GET_STARSHIP = gql`
-  query getStarship($id: ID!) {
-    starship(id: $id) {
-      id
-      name
-      model
-      starshipClass
-      manufacturers
-      length
-      crew
-      costInCredits
-      consumables
-      filmConnection {
-        films {
-          id
-          title
-        }
-      }
-    }
-  }
-`;
-*/
-
