db2f3d5efc43f816a2a693225722c7e5da28fb42
Finian Rawson <finrawson@gmail.com>
2021-04-13 16:30:58 -0700

    add instructor home page

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/Home';
+import InstructorHome from '../screens/Instructor/InstructorHome';
 import ClassPage from '../screens/ClassPage';
 import QuizPage from '../screens/Quiz/QuizPage';
 import QuizVideo from '../screens/Quiz/QuizVideo';
@@ -13,6 +14,7 @@ export default function App({ signOut }) {
   return (
     <AppStack.Navigator>
       <AppStack.Screen name="Home" component={Home} signOut={signOut}/>
+      <AppStack.Screen name="InstructorHome" component={InstructorHome} signOut={signOut}/>
       <AppStack.Screen name="ClassPage" component={ClassPage}/>
       <AppStack.Screen name="QuizPage" component={QuizPage}/>
       <AppStack.Screen name="QuizVideo" component={QuizVideo}/>
