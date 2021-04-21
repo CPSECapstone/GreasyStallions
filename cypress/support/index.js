5623eb407e98cc6553ad155823f85230573816fd
Abhishu Rawka <abhishurawka22@gmail.com>
2021-04-20 17:31:43 -0700

    Big Moves

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import Amplify, { Auth, Hub } from 'aws-amplify';
+import '@cypress/code-coverage/support'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
