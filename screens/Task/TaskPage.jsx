7728c04ceaf7ad7d0c832d2debb3bdf7278be6fc
Titouan Magret <titouan.magret@gmail.com>
2021-04-22 13:34:26 -0700

    refactored tasks added plain text task and changed some ui componenets

--- screens/Task/TaskPage.jsx
+++ screens/Task/TaskPage.jsx
@@ -1,17 +1,25 @@
-import { Form, Button, FormLabel, ButtonGroup, ToggleButton, Pagination } from 'react-bootstrap';
-import React, { useEffect } from 'react';
+import { Button, ButtonGroup, Pagination } from 'react-bootstrap';
+import React from 'react';
 import { View } from 'react-native';
 import QuizTask from './QuizTask';
 import VideoTask from './VideoTask';
 import RubricModal from './RubricModal';
+import TextPageTask from './TextPageTask';
 
+/**
+ * The general task page that will hold all components that define a task
+ * This page displayed general information about the task and has a slot for 
+ * a component that is the task item itself
+ */
 
-let TaskPage = ({ navigation }) =>{
+let TaskPage = ({ navigation }) => {
     const [taskNum, setTaskNum] = React.useState(1);
-    const [showRubric, setShowRubric] = React.useState(false);
+    const [showRubricModal, setShowRubricModal] = React.useState(false);
 
     let genTask;
-    let pageItems = [];
+    let showRubric = false; // whether or not rubric button is active
+    let rubric; // rubric object to be filled
+    let navBarItems = [];
 
     let changeTaskNum = (type) => {
         if (type === "+") {
@@ -34,14 +42,18 @@ let TaskPage = ({ navigation }) =>{
             task_info: "Today we are going to be watching a short video followed by a quiz and finally have a few free response questions"
         },
         {
+            task_title: "Week 1 General information",
+            task_type: "text",
+            task_text: "Hey class here is some information about what we are going to be \n" +
+             "doing in the coming week..."
+        },
+        {
             task_title: "Video for chapter 1",
-            task_num: 1,
             task_type: "video",
             task_video: "https://www.youtube.com/embed/TUZ_T_Rfios"
         },
         {
             task_title: "Follow up questions for video",
-            task_num: 2,
             task_type: "quiz",
             task_questions: ["ques 1", " test 2", "test 3 question"],
             task_options: [["option 1", "option 2"], ["option 1", "option 2", "option 3"],
@@ -51,31 +63,58 @@ let TaskPage = ({ navigation }) =>{
     ];
 
 
+    // look through task to find rubric, if its there enable rubric button
+    let findRubric = (tasks) => {
+        for (let task of tasks) {
+            if (task.task_type === "rubric") {
+                rubric = {
+                    task_title: task.task_title,
+                    task_count: task.task_count,
+                    task_points: task.task_points,
+                    task_type: task.task_type,
+                    task_info: task.task_info
+                }
+                showRubric = true;
+            }
+        }
+    }
+
+
+    // fill out the nav bar for the task
     for (let number = 1; number < task.length; number++) {
-        pageItems.push(
+        navBarItems.push(
             <Pagination.Item key={number} onClick={() => setTaskNum(number)}>
                 {task[number].task_title}
             </Pagination.Item>
         )
     }
 
-    if (task[taskNum].task_type == "video") {
+    if (task[taskNum].task_type === "video") {
         genTask = <VideoTask id={task[taskNum].task_video}/>
-    } else if  (task[taskNum].task_type == "quiz") {
+    } else if  (task[taskNum].task_type === "quiz") {
         genTask = <QuizTask questions={task[taskNum].task_questions}
          options={task[taskNum].task_options}
          answers={task[taskNum].answers}/>
+    } else if (task[taskNum].task_type === "text") {
+        genTask = <TextPageTask text={task[taskNum].task_text}/>
     }
 
     return (
         <View>
-            <Pagination>{pageItems}</Pagination>
-            <Button variant="primary" onClick={() => setShowRubric(true)}>Rubric</Button>
-            <h2>{task[taskNum].task_num + "/" + task.length + ": " + task[taskNum].  task_title}</h2>
+            {findRubric(task)}
+            <Pagination>{navBarItems}</Pagination>
+            <Button disabled={showRubricModal} variant="primary" onClick={() => setShowRubricModal(true)}>Rubric</Button>
+            <h2>{taskNum + "/" + (task.length - 1) + ": " + task[taskNum].task_title}</h2>
             {genTask}
             <ButtonGroup size="lg" classname="navbuts">
-                <Button variant="primary" onClick={() => changeTaskNum("-")}>Prev</Button>
-                {(taskNum === (task.length - 1)) ? 
+                <Button 
+                 variant="primary" 
+                 onClick={() => changeTaskNum("-")}
+                 disabled={!(taskNum != 1)}
+                >
+                    Prev
+                </Button>
+                {(taskNum === (task.length-1)) ? 
                 <Button  variant="primary" onClick={() => changeTaskNum("+")}>
                     Submit
                 </Button> : 
@@ -84,12 +123,12 @@ let TaskPage = ({ navigation }) =>{
                 </Button>}
             </ButtonGroup>
             <RubricModal 
-             show={showRubric}
-             close={() => setShowRubric(false)}
-             title={task[0].task_title}
-             points={task[0].task_points}
-             count={task[0].task_count}
-             info={task[0].task_info}
+             show={showRubricModal}
+             close={() => setShowRubricModal(false)}
+             title={rubric.task_title}
+             points={rubric.task_points}
+             count={rubric.task_count}
+             info={rubric.task_info}
             />
         </View>
     );
--- screens/Task/VideoTask.jsx
+++ screens/Task/VideoTask.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { iframe } from 'react-bootstrap';
 import YoutubePlayer from 'react-native-youtube-iframe';
 
+/**
+ * Displayes a youtube video, note: the link must be for the emebeded format not 
+ * the gereral url 
+ */
+
 let VideoTask = function({ id }) {
 
    return (
       <div class="embed-responsive embed-responsive-16by9">
-         <iframe width="1280" height="720" class="embed-responsive-item" 
+         <iframe width="1024" height="576" class="embed-responsive-item" 
           src={id} allowFullScreen autoplay></iframe>
       </div>
    );
