a4d5974d184d30835b914a5ea43296b016fd0bc6
arthurknatt <arthurknatt@gmail.com>
2021-04-22 17:40:59 -0700

    fixing spacing

--- screens/Task/FreeResponseTask.jsx
+++ screens/Task/FreeResponseTask.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 
-
 /*
-   free response quiz task
+   Free response question task page.
+   Still need to save the answer after submission.
 */
 
 let freeResponseAnswer = "";
@@ -12,16 +12,12 @@ let FreeResponseTask = function({ freeResponseQuestion }) {
 
    return (
       <Form>
-         <Form.Group controlId="formBasicEmail">
-         <Form.Label> {freeResponseQuestion} </Form.Label>
-         <Form.Control placeholder="Type your response here..." />
+         <Form.Group controlId="freeResponseTextArea">
+            <Form.Label> {freeResponseQuestion} </Form.Label>
+            <Form.Control as="textarea" rows={6} placeholder="Type your response here..." />
          </Form.Group>
       </Form>
    );
 }
-
-
-
-
 
 export default FreeResponseTask;
