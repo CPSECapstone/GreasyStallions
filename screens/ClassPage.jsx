4cd97105239d94512c3b36651cde5543ce8477c3
DJ <33232754+djuliano14@users.noreply.github.com>
2021-05-13 15:28:33 -0700

    create mission

--- screens/ClassPage.jsx
+++ screens/ClassPage.jsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import { ApolloProvider, useQuery, gql} from '@apollo/client';
 import {Typography, Grid, Box, Paper, List, ListItem, ListItemText,  Button} from '@material-ui/core';
 import randomColor from 'randomcolor';
+import CreateMission from './CreateModals/CreateMission';
 
 
 let ClassPage = function({ route, navigation }){
@@ -105,8 +106,10 @@ let ClassPage = function({ route, navigation }){
             {description}
           </Box>
         </Typography>
+		
 		<Typography style={{marginLeft:24, marginTop:36}} variant="h4">
           <Box fontWeight="fontWeightBold" m={1}>
+		  	<CreateMission/>
             Missions
           </Box>
         </Typography>
