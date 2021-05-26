840f1b79c66803bea7191dc7f36a6f58b465b59a
Abhishu Rawka <abhishurawka22@gmail.com>
2021-05-25 17:20:38 -0700

    Progress

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
+// import Amplify, { Auth, Hub } from 'aws-amplify';
+import {Dialog} from 'react-native-material-ui';
+import { apolloClient } from './apollo';
+import { apolloClientFlipted} from './apollo-flipted';
+import fliptedlogo from './assets/rn-logo.png';
+import fullfliptedlogo from './assets/rn-logo.png';
+// import config from './amplify/config';
+
 import React, { useState } from 'react';
 import { Button, Text, View, SafeAreaView, ActivityIndicator, StyleSheet } from 'react-native';
 import { ApolloProvider, useQuery, gql} from '@apollo/client';
@@ -5,9 +13,6 @@ import { StatusBar } from 'expo-status-bar';
 import AppNavigation from './navigation';
 import config from './aws-exports';
 import Amplify from '@aws-amplify/core';
-
-import { apolloClient } from './apollo';
-import { apolloClientFlipted} from './apollo-flipted';
 
 Amplify.configure(config);
 
