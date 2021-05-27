43147591a4a4922e4c2d9d181a6d565996455089
Titouan Magret <titouan.magret@gmail.com>
2021-05-27 15:15:52 -0700

    added view of missions

--- screens/ClassPage.jsx
+++ screens/ClassPage.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import {Button, View, Text, StyleSheet } from 'react-native';
+import { ApolloProvider, useQuery, gql } from '@apollo/client';
+import MissionsView from './Student/Mission/MissionsView';
 
-export default function ClassPage() {
+export default function ClassPage({route, navigation}) {
+	const { className = "Integrated Science"} = route.params;
+	const [curr, setCurr] = React.useState(0);
+	const COURSE_CONTENT = gql 
+	`
+		query {
+			courseContent(course: "${className}") {
+				courseInfo {
+					instructor description course
+				}
+				missions {
+					name
+					id
+				}
+				targets {
+					targetName
+				}
+			}
+		}
+	`;
+
+	let displayCurr = () => {
+		if (curr === 0) {
+			return <MissionsView className={className}/>
+		} else if (curr === 1) {
+			return <LearningTaskVisualization className={className}/>
+		}
+	}
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Classpage</Text>
-    </View>
+		<Text style={styles.text}>Classpage</Text>
+		<Text style={styles.titleText}>{(curr === 0) ? "Missions Progress" : "Mastery Progress"}</Text>
+		{displayCurr()}	
+	</View>
   )
 }
 
@@ -17,5 +50,8 @@ const styles = StyleSheet.create({
 	},
 	text: {
 	  textAlign: 'center'
+	},
+	titleText : {
+		fontWeight: 'bold'
 	},
   });
